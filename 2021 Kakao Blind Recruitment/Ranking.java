a7709d1ac07af865461151d9875fc886d3a7b9bd
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-03-25 01:10:12 +0900

    remove static keywords

--- 2021 Kakao Blind Recruitment/Ranking.java
+++ 2021 Kakao Blind Recruitment/Ranking.java
@@ -1,27 +1,27 @@
+// 2021 카카오 블라인드 채용
+// 순위 검색
+// 2021.03.24
+
 import java.util.*;
 
-public class Ranking {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new String[]{"java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"},
-                new String[]{"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"})));
-    }
-
+class Solution {    
+    
     private static final int LANGUAGE = 0;
     private static final int POSITION = 1;
     private static final int CAREER = 2;
     private static final int SOUL_FOOD = 3;
     private static final int TEST_SCORE = 4;
 
-    private static Set<Applicant> applicants = new HashSet<>();
-    private static List<Integer> queryResults = new ArrayList<>();
+    private Set<Applicant> applicants = new HashSet<>();
+    private List<Integer> queryResults = new ArrayList<>();
 
-    public static int[] solution(String[] infos, String[] queries) {
+    public int[] solution(String[] infos, String[] queries) {
         parseApplicantsInfo(infos);
         queryForResults(queries);
         return queryResults.stream().mapToInt(Integer::valueOf).toArray();
     }
 
-    private static void parseApplicantsInfo(String[] infos) {
+    private void parseApplicantsInfo(String[] infos) {
         for (String info : infos) {
             String[] chunks = info.split(" ");
             applicants.add(new Applicant(new Condition(
@@ -30,7 +30,7 @@ public class Ranking {
         }
     }
 
-    private static void queryForResults(String[] queries) {
+    private void queryForResults(String[] queries) {
         for (String query : queries) {
             Condition condition = parseQueryToCondition(query);
             queryResults.add((int) applicants.stream()
@@ -39,7 +39,7 @@ public class Ranking {
         }
     }
 
-    private static Condition parseQueryToCondition(String query) {
+    private Condition parseQueryToCondition(String query) {
         String[] conditions = query.split("and");
         int testScore = parseTestScore(conditions[SOUL_FOOD].strip().split(" ")[1]);
         String soulFood = conditions[SOUL_FOOD].strip().split(" ")[0];
@@ -47,7 +47,7 @@ public class Ranking {
                 conditions[CAREER].strip(), soulFood.strip(), testScore);
     }
 
-    private static int parseTestScore(String str) {
+    private int parseTestScore(String str) {
         if (str.equals(Condition.NULL)) return Condition.NO_TEST_SCORE;
         else return Integer.parseInt(str);
     }
