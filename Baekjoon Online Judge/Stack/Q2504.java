87dc95dec0c485c1a5125df9df7d39beee903c75
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-04-09 02:45:21 +0900

    update Q2504: add another solution

--- Baekjoon Online Judge/Stack/Q2504.java
+++ Baekjoon Online Judge/Stack/Q2504.java
@@ -1,11 +1,11 @@
 // 백준 2504번 괄호의 값
 // Stack
-// 2021.04.06
+// 2021.04.06, 2021.04.08
 
 import java.io.*;
 import java.util.Stack;
 
-public class Q2504 {
+public class Q2504_ver1 {
 
     private static BufferedReader br;
     private static BufferedWriter bw;
@@ -61,5 +61,79 @@ public class Q2504 {
         if (current == ')' && stack.peek() != '(') return true;
         if (current == ']' && stack.peek() != '[') return true;
         return false;
+    }
+}
+
+public class Q2504_ver2 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line = br.readLine();
+        int answer = calcValue(line);
+        bw.append(Integer.toString(answer));
+
+        br.close();
+        bw.close();
+    }
+
+    private static int calcValue(String line) {
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < line.length(); i++) {
+            char current = line.charAt(i);
+            if (isOpeningParentheses(current)) {
+                stack.push(current + "");
+            } else if (isClosingParentheses(current)) {
+                int subValue = Math.max(getSubValue(stack), 1);
+                if (stack.isEmpty() || !stack.pop().equals(pair(current))) return 0;
+                stack.push(subValue * unit(current) + "");
+            }
+        }
+        return getResultValue(stack);
+    }
+
+    private static boolean isOpeningParentheses(char current) {
+        return current == '(' || current == '[';
+    }
+
+    private static boolean isClosingParentheses(char current) {
+        return current == ')' || current == ']';
+    }
+
+    private static int getSubValue(Stack<String> stack) {
+        int result = 0;
+        while (!stack.isEmpty() && isNumber(stack.peek()))
+            result += Integer.parseInt(stack.pop());
+        return result;
+    }
+
+    private static boolean isNumber(String str) {
+        return !str.equals("(") && !str.equals(")")
+                && !str.equals("[") && !str.equals("]");
+    }
+
+    private static String pair(char current) {
+        if (current == ')') return "(";
+        else return "[";
+    }
+
+    private static int unit(char current) {
+        if (current == ')') return 2;
+        else return 3;
+    }
+
+    private static int getResultValue(Stack<String> stack) {
+        int result = 0;
+        while (!stack.isEmpty()) {
+            String current = stack.pop();
+            if (!isNumber(current)) return 0;
+            else result += Integer.parseInt(current);
+        }
+        return result;
     }
 }
