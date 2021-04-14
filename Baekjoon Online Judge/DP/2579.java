3613f62d357cb3046b5f174cc762c724e83ac672
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-04-14 14:33:05 +0900

    update Q2579

--- Baekjoon Online Judge/DP/2579.java
+++ Baekjoon Online Judge/DP/2579.java
@@ -1,48 +1,37 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-// 백준 2579 '계단 오르기'
+// 백준 2579번 계단 오르기
 // DP
-// 2020.07.20
+// 2021.04.14
+
+import java.io.*;
 
 public class Main {
 
-	static int N;
-	static int cost[];
-	static int maxCost[];
+    private static final int MAXIMUM = 300;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int n;
+    private static int[] costs = new int[MAXIMUM + 1];
+    private static int[] dp = new int[MAXIMUM + 1];
 
-		StringTokenizer tk = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		N = Integer.parseInt(tk.nextToken());
-		cost = new int[N + 10];
-		maxCost = new int[N + 10];
+        n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++)
+            costs[i] = Integer.parseInt(br.readLine());
 
-		int n;
-		for (int i = 1; i < N + 1; i++) {
-			tk = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(tk.nextToken());
-			cost[i] = n;
-		}
+        dp();
+        bw.append(String.valueOf(dp[n]));
 
-		maxCost[0] = 0;
-		maxCost[1] = cost[1];
-		maxCost[2] = cost[1] + cost[2];
-		for (int i = 3; i < N + 1; i++) {
-			maxCost[i] = Math.max(cost[i] + cost[i - 1] + maxCost[i - 3], cost[i] + maxCost[i - 2]);
-		}
+        br.close();
+        bw.close();
+    }
 
-		bw.write(Integer.toString(maxCost[N]));
-
-		br.close();
-		bw.close();
-	}
+    private static void dp() {
+        dp[1] = costs[1];
+        dp[2] = costs[1] + costs[2];
+        for (int step = 3; step <= n; step++)
+            dp[step] = Math.max(dp[step - 3] + costs[step - 1], dp[step - 2])
+                    + costs[step];
+    }
 }
