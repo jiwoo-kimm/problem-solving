33e70c9826631a79ecbeee49acbe730463d65f35
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-04-15 14:10:05 +0900

    update Q1912

--- Baekjoon Online Judge/DP/1912.java
+++ Baekjoon Online Judge/DP/1912.java
@@ -1,43 +1,42 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-// 백준 1912 '연속합'
+// 백준 1912번 연속합
 // DP
-// 2020.07.25
+// 2020.07.25, 2021.04.15
+
+import java.io.*;
 
 public class Main {
 
-	static int N;
-	static int cost[];
-	static int res = Integer.MIN_VALUE;
+    private static final int MAXIMUM = 100000;
 
-	public static void main(String[] args) throws IOException {
+    private static int n;
+    private static long[] nums = new long[MAXIMUM + 1];
+    private static long[] dp = new long[MAXIMUM + 1];
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer tk = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        String[] line = br.readLine().split(" ");
+        for (int i = 1; i <= n; i++)
+            nums[i] = Integer.parseInt(line[i - 1]);
 
-		N = Integer.parseInt(tk.nextToken());
-		cost = new int[N + 1];
+        dp();
+        bw.append(String.valueOf(findMax()));
 
-		cost[0] = 0;
-		tk = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			cost[i] = Integer.parseInt(tk.nextToken());
-			if (cost[i - 1] + cost[i] > cost[i])
-				cost[i] += cost[i - 1];
-			if (cost[i] > res)
-				res = cost[i];
-		}
+        br.close();
+        bw.close();
+    }
 
-		bw.write(Integer.toString(res));
+    private static void dp() {
+        dp[1] = nums[1];
+        for (int i = 2; i <= n; i++)
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    }
 
-		br.close();
-		bw.close();
-	}
+    private static long findMax() {
+        long max = Long.MIN_VALUE;
+        for (int i = 1; i <= n; i++) max = Math.max(max, dp[i]);
+        return max;
+    }
 }
