a1d382a778531806758632e9bc62e1d2a4898016
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-04-30 04:18:16 +0900

    add Q1994

--- Baekjoon Online Judge/Binary Search/Q1994.java
+++ Baekjoon Online Judge/Binary Search/Q1994.java
@@ -1,18 +1,15 @@
-package bs;
-
 import java.io.*;
 import java.util.Arrays;
 
-public class Q1994 {
+public class Main {
 
-    private static final int MIN_SIZE = 1;
     private static final int MAX_SIZE = 2000;
-    private static final int MAX_VALUE = 1000000000;
     private static final int INVALID = -1;
+    private static final int INIT = 0;
 
     private static int n;
-    private static int[] arr = new int[MAX_SIZE];
-    private static int[][] dp = new int[MAX_SIZE][MAX_SIZE];
+    private static int[] arr = new int[MAX_SIZE + 1];
+    private static int[][] dp = new int[MAX_SIZE + 1][MAX_SIZE + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,37 +25,37 @@ public class Q1994 {
 
     private static void parseInput(BufferedReader br) throws IOException {
         n = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(br.readLine());
-        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) arr[i + 1] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr, 1, n + 1);
     }
 
     private static int findLongestApLength() {
-        if (n == MIN_SIZE) return MIN_SIZE;
-
-        Arrays.fill(dp[1], 2);
-        for (int i = 2; i <= n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                int gap = arr[j] - arr[i];
-                if (gap == 0) {
-
-                } else {
-                    int next = findIndexNext(j, gap);
-                    if (next == INVALID) dp[i][j] = 2;
-                    else dp[i][j] = dp[j][next] + 1;
-                }
-            }
-        }
+        int max = 1;
+        for (int i = 1; i <= n; i++)
+            for (int j = i + 1; j <= n; j++)
+                max = Math.max(max, dp(i, j));
+        return max;
     }
 
-    private static int findIndexNext(int index, int gap) {
-        int left = index + 1, right = n, mid;
-        while (left < right) {
+    private static int dp(int i, int j) {
+        int result = dp[i][j];
+        if (result != INIT) return result;
+
+        int index = findIndex(arr[j] + arr[j] - arr[i], j + 1);
+
+        if (index == INVALID) return dp[i][j] = 2;
+        else return dp[i][j] = dp(j, index) + 1;
+    }
+
+    private static int findIndex(int target, int start) {
+        int left = start, right = n, mid;
+        while (left <= right) {
             mid = (left + right) / 2;
-            if (arr[mid] == arr[index] + gap) left = mid + 1;
+            if (arr[mid] == target) return mid;
+
+            if (arr[mid] < target) left = mid + 1;
             else right = mid - 1;
         }
-        int result = (left + right) / 2;
-        if (arr[result] != arr[index] + gap) result = INVALID;
-        return result;
+        return INVALID;
     }
 }
