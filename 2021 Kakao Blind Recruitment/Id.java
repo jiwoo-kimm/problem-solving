9d8e9080d9d48bb34a1e44844c4306ce96f3ccaa
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-05-07 15:25:03 +0900

    refactor code and add comments

--- 2021 Kakao Blind Recruitment/Id.java
+++ 2021 Kakao Blind Recruitment/Id.java
@@ -1,56 +1,34 @@
-package kakaoblind2021;
+// 2021 카카오 블라인드 채용 : 신규 아이디 추천
+// 2021.05.07
 
-public class Id {
-
+class Solution {
+    
     private static final char DOT = '.';
+    private static final String BLANK = "";
     private static final String NEW_ID_FOR_EMPTY_STRING = "a";
     private static final int MAX_LENGTH = 15;
     private static final int MIN_LENGTH = 3;
-
-    public static void main(String[] args) {
-        System.out.println(solution("...!@BaT#*..y.abcdefghijklm"));
-//        System.out.println(solution("z-+.^."));
-        System.out.println(solution("=.="));
-        System.out.println(solution("123_.def"));
-        System.out.println(solution("abcdefghijklmn.p"));
-    }
-
-    private static String solution(String new_id) {
+    
+    public String solution(String new_id) {
         // 1단계
         String result = new_id.toLowerCase();
-        System.out.println(result);
         // 2단계
-        result = result.replaceAll("[^-_.a-z0-9]", "");
-        System.out.println(result);
+        result = result.replaceAll("[^-_.a-z0-9]", BLANK);
         // 3단계
-        result = result.replaceAll("[.]{3}|[.]{2}", DOT + "");
-        System.out.println(result);
+        result = result.replaceAll("[.]{2,}", DOT + BLANK);
         // 4단계
-        result = removeDotAtFrontAndBack(result);
-        System.out.println(result);
+        result = result.replaceAll("^[.]|[.]$", BLANK);
         // 5단계
         result = (result.isEmpty() ? NEW_ID_FOR_EMPTY_STRING : result);
-        System.out.println(result);
         // 6단계
-        result = (result.length() > MAX_LENGTH ? removeDotAtFrontAndBack(result.substring(0, MAX_LENGTH)) : result);
-        System.out.println(result);
+        result = (result.length() > MAX_LENGTH ? result.substring(0, MAX_LENGTH).replaceAll("^[.]|[.]$", BLANK) : result);
         // 7단계
         result = (result.length() < MIN_LENGTH ? extendToMinLength(result) : result);
-        System.out.println(result);
 
         return result;
     }
 
-    private static String removeDotAtFrontAndBack(String result) {
-        if (result.charAt(0) == DOT) result = result.substring(1);
-
-        if (result.length() > 0 && result.charAt(result.length() - 1) == DOT)
-            result = result.substring(0, result.length() - 1);
-
-        return result;
-    }
-
-    private static String extendToMinLength(String result) {
+    private String extendToMinLength(String result) {
         char ch = result.charAt(result.length() - 1);
         while (result.length() < MIN_LENGTH) result += ch;
         return result;
