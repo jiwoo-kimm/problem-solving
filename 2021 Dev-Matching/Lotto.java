b983cbe92abbceda09365f47ea5c0952ce7b309d
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2021-05-18 15:36:09 +0900

    update comment

--- 2021 Dev-Matching/Lotto.java
+++ 2021 Dev-Matching/Lotto.java
@@ -1,27 +1,15 @@
-package devmatching2021;
+// 2021 Dev-Matching 웹 백엔드 개발자(상반기)
+// 로또의 최고 순위와 최저 순위
+// 2021.05.18
 
 import java.util.Arrays;
 
-public class Lotto {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(
-                new int[]{44, 1, 0, 0, 31, 25},
-                new int[]{31, 10, 45, 1, 6, 19}
-        )));
-        System.out.println(Arrays.toString(solution(
-                new int[]{0, 0, 0, 0, 0, 0},
-                new int[]{38, 19, 20, 40, 15, 25}
-        )));
-        System.out.println(Arrays.toString(solution(
-                new int[]{45, 4, 35, 20, 3, 9},
-                new int[]{20, 9, 3, 45, 4, 35}
-        )));
-    }
-
+class Solution {
+    
     private static final int UNKNOWN = 0;
     private static final int SIZE = 6;
 
-    public static int[] solution(int[] lottos, int[] win_nums) {
+    public int[] solution(int[] lottos, int[] win_nums) {
         Arrays.sort(lottos);
         Arrays.sort(win_nums);
 
@@ -48,7 +36,7 @@ public class Lotto {
         return parseAnswer(lowerBound, lowerBound + zeroCount);
     }
 
-    private static int[] parseAnswer(int lowerBound, int upperBound) {
+    private int[] parseAnswer(int lowerBound, int upperBound) {
         int[] result = new int[2];
         result[0] = Math.min(6, 7 - upperBound);
         result[1] = Math.min(6, 7 - lowerBound);
