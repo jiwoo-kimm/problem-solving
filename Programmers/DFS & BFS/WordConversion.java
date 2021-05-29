6babe7c6d9392b45a63b6f802ecd9b41ac4cfc79
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-05-29 21:32:38 +0900

    update WordConversion

--- Programmers/DFS & BFS/WordConversion.java
+++ Programmers/DFS & BFS/WordConversion.java
@@ -1,46 +1,48 @@
 // 프로그래머스 '단어 변환'
 // DFS & BFS
-// 2021.01.05
+// 2021.01.05, 2021.05.29
 
-import java.util.Arrays;
+import java.util.*;
 
-public class WordConversion {
-
-    public static void main(String[] args) {
-        System.out.println(solution("hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log", "cog"}));
-    }
-
-    private static int answer = Integer.MAX_VALUE;
-
-    public static int solution(String begin, String target, String[] words) {
+class Solution {
+    
+    public int solution(String begin, String target, String[] words) {
         boolean[] visited = new boolean[words.length];
-        Arrays.fill(visited, false);
-        dfs(target, words, visited, begin, 0);
-        if (answer == Integer.MAX_VALUE)
-            answer = 0;
-        return answer;
-    }
-
-    private static void dfs(String target, String[] words, boolean[] visited, String current, int count) {
-        if (current.equals(target)) {
-            if (answer > count)
-                answer = count;
-            return;
+        Queue<ConvertedWord> queue = new LinkedList<>();
+        queue.offer(new ConvertedWord(begin, 0));
+        while(!queue.isEmpty()) {
+            ConvertedWord current = queue.poll();
+            if (current.word.equals(target)) return current.count;
+            
+            for (int i=0 ; i<words.length ; i++) {
+                if (visited[i]) continue;
+                
+                if (current.isConvertable(words[i])) {
+                    visited[i] = true;
+                    queue.offer(new ConvertedWord(words[i], current.count+1));
+                }
+            }            
         }
-
-        for (int i = 0; i < words.length; i++)
-            if (isConvertible(words[i], current) && !visited[i]) {
-                boolean[] newVisited = Arrays.copyOf(visited, visited.length);
-                newVisited[i] = true;
-                dfs(target, words, newVisited, words[i], count + 1);
-            }
+        return 0;
     }
+}
 
-    private static boolean isConvertible(String current, String word) {
-        int diff = 0;
-        for (int i = 0; i < current.length(); i++)
-            if (current.charAt(i) != word.charAt(i))
-                diff++;
-        return diff == 1;
+class ConvertedWord {
+    String word;
+    int count;
+    
+    public ConvertedWord(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+    
+    public boolean isConvertable(String target) {
+        if (word.length() != target.length()) return false;
+        int count = 0;
+        for (int i=0 ; i<word.length() ; i++) {
+            if (word.charAt(i) != target.charAt(i))
+                if (++count > 1) return false;
+        }
+        return true;
     }
 }
