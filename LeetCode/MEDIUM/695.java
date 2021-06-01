1c328051ecafb897b9153697b18d485575d06b96
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-06-02 01:23:55 +0900

    update 695

--- LeetCode/MEDIUM/695.java
+++ LeetCode/MEDIUM/695.java
@@ -59,3 +59,33 @@ class Island {
         this.x=x;
     }
 }
+
+
+
+// DFS
+
+class Solution {
+    
+    private static final int VALID = 1;
+    private static final int INVALID = 0;
+    
+    public int maxAreaOfIsland(int[][] grid) {
+        
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int max = 0;
+        for (int i=0 ; i<grid.length ; i++)
+            for (int j=0 ; j<grid[0].length ; j++)
+                if (!visited[i][j] && grid[i][j] == VALID)
+                    max = Math.max(max, dfs(i, j, grid, visited));
+        return max;
+    }
+    
+    private int dfs(int i, int j, int[][] grid, boolean[][] visited) {
+        if (i < 0 || i >= grid.length || j < 0 || j>= grid[0].length) return 0;
+        if (grid[i][j] == INVALID || visited[i][j]) return 0;
+        
+        visited[i][j] = true;
+        return dfs(i+1, j, grid, visited) + dfs(i-1, j, grid, visited)
+                + dfs(i, j+1, grid, visited) + dfs(i, j-1, grid, visited) + 1;
+    }
+}
