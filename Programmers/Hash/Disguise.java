bfad30de185a345883725f694c38ea3bb3e59440
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-24 14:47:37 +0900

    update Disguise

--- Programmers/Hash/Disguise.java
+++ Programmers/Hash/Disguise.java
@@ -1,23 +1,20 @@
 // 프로그래머스 '위장'
 // 해시
-// 2021.01.21
+// 2021.01.21, 2021.08.24
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class Disguise {
-
-    public static void main(String[] args) {
-        System.out.println(solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
-        System.out.println(solution(new String[][]{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
-    }
-
-    private static final int CATEGORY = 1;
-
-    public static int solution(String[][] clothes) {
-        Map<String, Integer> collection = new HashMap<>();
-        for (String[] element : clothes)
-            collection.put(element[CATEGORY], collection.getOrDefault(element[CATEGORY], 0) + 1);
-        return collection.values().stream().mapToInt(value -> value + 1).reduce(1, (a, b) -> a * b) - 1;
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> counts = new HashMap<>();
+        
+        for (String[] each : clothes) {
+            String category = each[1];
+            counts.put(category, counts.getOrDefault(category, 0) + 1);
+        }
+        
+        return counts.values().stream()
+            .mapToInt(count -> count+1)
+            .reduce(1, (a,b) -> a*b) - 1;
     }
 }
