092305728de2a0fe061dfc2b99e0bf0b8a0d61bf
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-24 13:16:15 +0900

    update Phone

--- Programmers/Hash/Phone.java
+++ Programmers/Hash/Phone.java
@@ -1,21 +1,19 @@
 // 프로그래머스 '전화번호 목록'
 // 해시
-// 2021.01.21
+// 2021.01.21, 2021.08.24
 
-import java.util.Arrays;
+import java.util.*;
 
-public class Phone {
-
-    public static void main(String[] args) {
-        System.out.println(solution(new String[]{"119", "97674223", "1195524421"}));
-        System.out.println(solution(new String[]{"123", "456", "789"}));
-        System.out.println(solution(new String[]{"12", "123", "1235", "567", "88"}));
-    }
-
-    public static boolean solution(String[] phone_book) {
-        for (String phone : phone_book)
-            if (Arrays.stream(phone_book).anyMatch(number -> !number.equals(phone) && number.startsWith(phone)))
+class Solution {
+    public boolean solution(String[] phone_book) {  
+        Arrays.sort(phone_book);
+        
+        for (int i=0 ; i<phone_book.length-1 ; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
                 return false;
+            }
+        }
+        
         return true;
     }
 }
