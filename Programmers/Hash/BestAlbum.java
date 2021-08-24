17fcd5fa806cdd6d2674974727cef488056e9c18
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-24 15:26:54 +0900

    update BestAlbum

--- Programmers/Hash/BestAlbum.java
+++ Programmers/Hash/BestAlbum.java
@@ -1,49 +1,40 @@
 // 프로그래머스 '베스트앨범'
 // 해시
-// 2021.01.21
+// 2021.01.21, 2021.08.24
 
 import java.util.*;
+import java.util.stream.*;
 
-public class BestAlbum {
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new String[]{"classic", "pop", "classic", "classic", "pop"}, new int[]{500, 600, 150, 800, 2500})));
-    }
-
-    public static int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> genreCounts = new HashMap<>();
-        Map<String, ArrayList<Song>> songsPerGenre = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) {
-            genreCounts.put(genres[i], genreCounts.getOrDefault(genres[i], 0) + plays[i]);
-            if (!songsPerGenre.containsKey(genres[i])) {
-                ArrayList<Song> newGenre = new ArrayList<>();
-                newGenre.add(new Song(i, genres[i], plays[i]));
-                songsPerGenre.put(genres[i], newGenre);
-            } else songsPerGenre.get(genres[i]).add(new Song(i, genres[i], plays[i]));
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        int n = genres.length;
+        
+        Set<String> distinctGenres = new HashSet<>();
+        Map<String, Integer> countPerGenre = new HashMap<>();
+        Map<String, PriorityQueue<Integer>> songsPerGenre = new HashMap<>();
+        
+        for (String each : genres) {
+            distinctGenres.add(each);
+            songsPerGenre.put(each, new PriorityQueue<>((o1, o2) -> plays[o2] - plays[o1]));
         }
-
-        ArrayList<Integer> answer = new ArrayList<>();
-        genreCounts.entrySet().stream()
-                .sorted(Comparator.comparingInt(entry -> -entry.getValue()))
-                .forEach(entry -> {
-                    ArrayList<Song> targets = songsPerGenre.get(entry.getKey());
-                    targets.sort(Comparator.comparingInt(o -> -o.play));
-                    answer.add(targets.get(0).index);
-                    if (targets.size() > 1) answer.add(targets.get(1).index);
-                });
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-}
-
-class Song {
-
-    int index;
-    String genre;
-    int play;
-
-    public Song(int index, String genre, int play) {
-        this.index = index;
-        this.genre = genre;
-        this.play = play;
+        
+        for (int i=0 ; i<n ; i++) {
+            countPerGenre.put(genres[i], countPerGenre.getOrDefault(genres[i], 0) + plays[i]);
+            songsPerGenre.get(genres[i]).offer(i);
+        }
+        
+        List<String> genresInOrder = new ArrayList<>(distinctGenres);
+        Collections.sort(genresInOrder, (o1, o2) -> countPerGenre.get(o2) - countPerGenre.get(o1));
+        
+        List<Integer> answer = new ArrayList<>();
+        for (String genre : genresInOrder) {
+            PriorityQueue<Integer> pq = songsPerGenre.get(genre);
+            answer.add(pq.poll());
+            if (!pq.isEmpty()) {
+                answer.add(pq.poll());
+            }
+        }
+        
+        return answer.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
