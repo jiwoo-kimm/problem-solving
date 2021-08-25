b7b93af292a2d6829acc96106529c1005340ce16
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-25 17:32:34 +0900

    add Printer

--- Programmers/Stack & Queue/Printer.java
+++ Programmers/Stack & Queue/Printer.java
@@ -1,43 +1,41 @@
 // 프로그래머스 '프린터'
-// 2021.02.16
+// 2021.02.16, 2021.08.25
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
-public class Printer {
-
-    public static void main(String[] args) {
-//        System.out.println(solution(new int[]{2, 1, 3, 2}, 2));
-        System.out.println(solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
-    }
-
-    private static Queue<Document> queue;
-    private static int answer;
-
-    public static int solution(int[] priorities, int location) {
-        queue = new LinkedList<>();
-        for (int i = 0; i < priorities.length; i++) queue.offer(new Document(priorities[i], i));
-        while (!queue.isEmpty()) {
-            Document head = queue.poll();
-            if (isPrintable(head)) {
-                answer++;
-                if (head.index == location) break;
-            } else queue.offer(head);
+class Solution {
+    public int solution(int[] priorities, int targetIndex) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int each : priorities){
+            queue.add(each);
         }
-        return answer;
-    }
 
-    private static boolean isPrintable(Document head) {
-        return queue.stream().noneMatch(document -> document.priority > head.priority);
-    }
-}
+        Arrays.sort(priorities);
+        int size = priorities.length;
 
-class Document {
-    int priority;
-    int index;
-
-    public Document(int priority, int index) {
-        this.priority = priority;
-        this.index = index;
+        int printedCount = 0;
+        while(!queue.isEmpty()){
+            int priority = queue.poll();
+            int maxPriority = priorities[size - printedCount - 1];
+            
+            if (priority == maxPriority) {
+                printedCount++;
+                
+                if (targetIndex == 0) {
+                    return printedCount;
+                } else {
+                    targetIndex--;
+                }
+            } else {
+                queue.offer(priority);
+                
+                if (targetIndex > 0) {
+                    targetIndex--;                    
+                } else {
+                    targetIndex = queue.size()-1;
+                }
+            }
+        }
+        return printedCount;
     }
 }
