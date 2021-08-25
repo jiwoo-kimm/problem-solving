89e77c3efb502a48949c803aaefd4fcafc682c04
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-25 17:32:42 +0900

    update FunctionDevelopment

--- Programmers/Stack & Queue/FunctionDevelopment.java
+++ Programmers/Stack & Queue/FunctionDevelopment.java
@@ -1,45 +1,53 @@
 // 프로그래머스 스택/큐
 // 기능개발
-// 2021.02.04
-
+// 2021.02.04, 2021.08.24
 
 import java.util.*;
 
-public class FunctionDevelopment {
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{93, 30, 55}, new int[]{1, 30, 5})));
-        System.out.println(Arrays.toString(solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1})));
-    }
-
-    public static int[] solution(int[] progresses, int[] speeds) {
-        Queue<Function> functions = new LinkedList<>();
-        for (int i = 0; i < progresses.length; i++) functions.offer(new Function(progresses[i], speeds[i]));
-
-        int day = 0, count = 0;
+class Solution {
+    
+    private static final int DONE = 100;
+    
+    private int[] progresses;
+    private int[] speeds;
+    private int total;
+    
+    public int[] solution(int[] progresses, int[] speeds) {
+        this.progresses = progresses;
+        this.speeds = speeds;
+        this.total = progresses.length;
+        
         List<Integer> answer = new ArrayList<>();
-        while (!functions.isEmpty()) {
-            while (functions.peek().progress + day * functions.peek().speed >= 100) {
-                functions.poll();
-                count++;
-                if (functions.isEmpty()) break;
+        
+        int work = 0;
+        while (work < total) {
+            int day = workDay(work);
+            int finished = 1;
+            
+            work++;
+            
+            while (work < total) {
+                if (workDay(work) > day) {
+                    break;
+                }
+                
+                finished++;
+                work++;
             }
-            if (count != 0) {
-                answer.add(count);
-                count = 0;
-            }
-            day++;
-        }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+            
+            answer.add(finished);
+       }
+        
+        return answer.stream().mapToInt(Integer::valueOf).toArray();
     }
-}
-
-class Function {
-    int progress;
-    int speed;
-
-    public Function(int progress, int speed) {
-        this.progress = progress;
-        this.speed = speed;
+    
+    private int workDay(int target) {
+        int remain = DONE - progresses[target];
+        int speed = speeds[target];
+        
+        if (remain % speed == 0) {
+            return remain / speed;
+        }
+        return remain / speed + 1;
     }
 }
