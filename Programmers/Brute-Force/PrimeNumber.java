23be3ea90ed51b8bec9d9bfa034b3ce1a24ecdf3
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-27 16:46:37 +0900

    update PrimeNumber

--- Programmers/Brute-Force/PrimeNumber.java
+++ Programmers/Brute-Force/PrimeNumber.java
@@ -1,59 +1,55 @@
 // 프로그래머스 '소수 찾기'
-// 2021.02.24
+// 2021.02.24, 2021.08.27
 
 import java.util.*;
 
 class Solution {
     
-    private static final int ASCII_BASE = 48;
-
-    private List<Integer> digits = new ArrayList<>();
     private Set<Integer> primeNumbers = new HashSet<>();
-
+    
     public int solution(String numbers) {
-        extractDigits(numbers);
-        makePrimeNumbers();
+        char[] digits = numbers.toCharArray();
+        
+        for (int i=0 ; i<digits.length ; i++) {
+            if (digits[i] == '0') {
+                continue;
+            }
+            
+            boolean[] visited = new boolean[digits.length];
+            visited[i] = true;
+            
+            permutation(""+digits[i], digits, visited);
+        }
+        
         return primeNumbers.size();
     }
-
-    private void extractDigits(String numbers) {
-        for (char number : numbers.toCharArray()) digits.add(number - ASCII_BASE);
-    }
-
-    private void makePrimeNumbers() {
-        boolean[] visited = new boolean[digits.size()];
-        int[] result = new int[digits.size()];
-        dfs(visited, result, 0);
-    }
-
-    private void dfs(boolean[] visited, int[] result, int depth) {
-        int number = makeNumber(result);
-        if (isPrimeNumber(number)) primeNumbers.add(number);
-
-        for (int i = 0; i < digits.size(); i++) {
+    
+    private void permutation(String current, char[] digits, boolean[] visited) {        
+        int number = Integer.parseInt(current) ;
+        
+        if (isPrime(number)) {
+            primeNumbers.add(number);
+        }
+        
+        for (int i=0 ; i<digits.length ; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                result[depth] = digits.get(i);
-                dfs(visited, result, depth + 1);
+                permutation(current+digits[i], digits, visited);
                 visited[i] = false;
-                result[depth] = 0;
             }
         }
     }
-
-    private int makeNumber(int[] result) {
-        int number = 0, unit = 1;
-        for (int digit : result) {
-            number += digit * unit;
-            unit *= 10;
+    
+    private boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
         }
-        return number;
-    }
-
-    private boolean isPrimeNumber(int number) {
-        if (number < 2) return false;
-        for (int i = 2; i <= Math.sqrt(number); i++)
-            if (number % i == 0) return false;
+        
+        for (int i=2 ; i<=Math.sqrt(number) ; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
