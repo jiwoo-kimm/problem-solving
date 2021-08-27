197ffd4d41f76314cf0c40f24a05b9548b8bd2d2
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-08-27 17:14:58 +0900

    update Carpet

--- Programmers/Brute-Force/Carpet.java
+++ Programmers/Brute-Force/Carpet.java
@@ -1,19 +1,21 @@
 // 프로그래머스 '카펫'
 // 완전탐색
-// 2021.03.03
+// 2021.03.03, 2021.08.27
 
 class Solution {
-    
     public int[] solution(int brown, int yellow) {
-        int yellowHeight = 1, yellowWidth = yellow / yellowHeight;
-        while (true) {
-            int brownGuess = (yellowWidth + 2) * 2 + yellowHeight * 2;
-            int yellowGuess = yellowWidth * yellowHeight;
-            if (brown == brownGuess && yellowGuess == yellow)
-                break;
-            yellowHeight++;
-            yellowWidth = yellow / yellowHeight;
+        int total = brown + yellow;
+        
+        for (int yellowHeight=1 ; yellowHeight<=Math.sqrt(yellow) ; yellowHeight++) {
+            if (yellow % yellowHeight == 0) {
+                int totalHeight = yellowHeight + 2;
+                int totalWidth = (yellow / yellowHeight) + 2;
+                
+                if ((totalHeight * totalWidth) == total) {
+                    return new int[]{totalWidth, totalHeight};
+                }
+            }
         }
-        return new int[]{yellowWidth + 2, yellowHeight + 2};
+        return null;
     }
 }
