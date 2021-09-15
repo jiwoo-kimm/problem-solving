e295d19e4b33a3ca05976f280c4594b02bbadb51
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-09-15 22:48:36 +0900

    update week7

--- Programmers/Weekly Challenge/week7.java
+++ Programmers/Weekly Challenge/week7.java
@@ -2,54 +2,29 @@
 // 7주차 입실 퇴실
 // 2021.09.15
 
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] enter, int[] leave) {
         int n = enter.length;
-        
-        int[] enterOrder = new int[n];
-        int[] leaveOrder = new int[n];
-        for (int i=0 ; i<n ; i++) {
-            enterOrder[enter[i]-1] = i;
-            leaveOrder[leave[i]-1] = i;
-        }
-        
         int[] answer = new int[n];
         
-        for (int i=0 ; i<n-1 ; i++) {
-            for (int j=i+1 ; j<n ; j++) {
-                if (enterOrder[i] < enterOrder[j] && leaveOrder[i] < leaveOrder[j]) {
-                    int laterEnter = enterOrder[j];
-                    int earlierLeave = leaveOrder[i];
-                    while (++laterEnter < n) {
-                        int person = enter[laterEnter];
-                        if (leaveOrder[person-1] < earlierLeave) {
-                            answer[i]++;
-                            answer[j]++;
-                            break;
-                        }
-                    }
+        int i = -1;
+        int j = 0;
+        Set<Integer> room = new HashSet<>();
+        while (j < n) {
+            if (room.contains(leave[j])) {
+                room.remove(leave[j]);
+                answer[leave[j] - 1] += room.size();
+                for (int person : room) {
+                    answer[person - 1]++;
                 }
-                
-                if (enterOrder[i] < enterOrder[j] && leaveOrder[i] > leaveOrder[j]) {
-                    answer[i]++;
-                    answer[j]++;
-                }
-                
-                if (enterOrder[i] > enterOrder[j] && leaveOrder[i] < leaveOrder[j]) {
-                    answer[i]++;
-                    answer[j]++;
-                }
-                
-                if (enterOrder[i] > enterOrder[j] && leaveOrder[i] > leaveOrder[j]) {
-                    int laterEnter = enterOrder[i];
-                    int earlierLeave = leaveOrder[j];
-                    while (++laterEnter < n) {
-                        int person = enter[laterEnter];
-                        if (leaveOrder[person-1] < earlierLeave) {
-                            answer[i]++;
-                            answer[j]++;
-                            break;
-                        }
+                j++;
+            } else {
+                while (++i < n) {
+                    room.add(enter[i]);
+                    if (enter[i] == leave[j]) {
+                        break;
                     }
                 }
             }
