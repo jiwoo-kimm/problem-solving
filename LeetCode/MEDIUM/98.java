acf65b071fecdd3369b97bcda79c2da5843454bb
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-09-30 01:23:47 +0900

    add leetcode 98

---
+++ LeetCode/MEDIUM/98.java
@@ -0,0 +1,21 @@
+// LeetCode
+// 98. Validate Binary Search Tree
+// 2021.09.30
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValid(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        
+        return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+    }
+}
